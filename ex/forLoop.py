3eb5d62cb2cc0be8b575701cef255d89caaa7c94
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-27 16:46:38 -0700

    more ex

---
+++ ex/forLoop.py
@@ -0,0 +1,10 @@
+friends = ["Ab", "Be", "Ce", "De"]
+
+# for index in range(len(friends)):
+#     print(index)
+
+for index in range(5):
+    if index == 0:
+        print('the first Iteration')
+    else:
+        print('the rest')
