28fdad362843452b525f9f08a002b8bf9d60b13c
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-11-03 15:46:50 -0700

    writing to file

---
+++ ex/writingToFiles.py
@@ -0,0 +1,32 @@
+# # open file with 'open('file', mode)' and store it inside var
+# random_file = open("random.txt", "a")  # to add some text to the end of the file
+
+# random_file.write("\nZzz - Sed iaculis lorem vel augue vulputate pulvinar eget sit amet magna.")
+# # print(random_file.read()) # print all file
+
+# random_file.close()
+
+# # "w" - overwrite entire file
+# random_file = open("random.txt", "w")  # to add some text to the end of the file
+
+# random_file.write("\nZzz - Sed iaculis lorem vel augue vulputate pulvinar eget sit amet magna.")
+# # print(random_file.read()) # print all file
+
+# random_file.close()
+
+
+# # Creating a new file - txt
+# random_file = open("random1.txt", "w")  # to add some text to the end of the file
+
+# random_file.write("\nZzz - Sed iaculis lorem vel augue vulputate pulvinar eget sit amet magna.")
+# # print(random_file.read()) # print all file
+
+# random_file.close()
+
+# # Creating a new file - HTML
+random_file = open("index.html", "w")  # to add some text to the end of the file
+
+random_file.write("<h1>Hello HTML</h1>")
+# print(random_file.read()) # print all file
+
+random_file.close()
