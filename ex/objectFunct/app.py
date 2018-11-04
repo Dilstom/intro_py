a11998af6326d67d07cd602e8dd876d43d8fe06f
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-11-04 15:16:52 -0800

    inheritance

---
+++ ex/objectFunct/app.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Oscar", "Business", 3.9)
+student2 = Student("Pam", "Art", 3.2)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
